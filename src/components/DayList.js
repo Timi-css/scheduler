ca435e18f23014e18102254ca6f2390e7c176133
Timi <alabitimi65@gmail.com>
2022-07-13 23:37:49 -0600

    added the days to the dayList

---
+++ src/components/DayList.js
@@ -0,0 +1,43 @@
+import React from "react";
+import DayListItem from "./DayListItem";
+
+export default function DayList(props) {
+  const { days, value, onChange } = props;
+
+  const dayItems = days.map((eachDay) => {
+    return (
+      <DayListItem
+        key={eachDay.id}
+        name={eachDay.name}
+        spots={eachDay.spots}
+        setDay={onChange}
+        selected={eachDay.name === value}
+      />
+    );
+  });
+  return (
+    <ul>
+      <DayListItem
+        key={props.days[0].id}
+        name={props.days[0].name}
+        spots={props.days[0].spots}
+        selected={props.days[0].name === props.day}
+        setDay={props.setDay}
+      />
+      <DayListItem
+        key={props.days[1].id}
+        name={props.days[1].name}
+        spots={props.days[1].spots}
+        selected={props.days[1].name === props.day}
+        setDay={props.setDay}
+      />
+      <DayListItem
+        key={props.days[2].id}
+        name={props.days[2].name}
+        spots={props.days[2].spots}
+        selected={props.days[2].name === props.day}
+        setDay={props.setDay}
+      />
+    </ul>
+  );
+}
---
+++ src/components/DayListItem.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "components/DayListItem.scss";
+import classNames from "classnames";
+
+export default function DayListItem(props) {
+  const { setDay, name, spots, selected } = props;
+  const dayListItemClass = classNames("DayListItem", {
+    "day-list__item": true,
+    "day-list__item--selected": selected,
+    "day-list__item--full": spots === 0,
+  });
+
+  function formatSpots(spots) {
+    if (spots > 1) {
+      return `${props.spots} spots remaining`;
+    }
+    if (spots === 1) {
+      return `1 spot remaining`;
+    }
+    if (spots === 0) {
+      return `no spots remaining`;
+    }
+  }
+
+  return (
+    <li className={dayListItemClass} onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{formatSpots(spots)}</h3>
+    </li>
+  );
+}
